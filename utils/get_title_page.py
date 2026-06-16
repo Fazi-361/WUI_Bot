16239bdbe37ad9a09457d2a079f6f03189c6cd52
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-06-16 11:42:37 +0200

    Migliora la gestione della translitterazione e dei tag così che non generino errori

--- utils/get_title_page.py
+++ utils/get_title_page.py
@@ -37,8 +37,8 @@ async def get_title_page(
     markdown: str = ""
     
     with sqlite3.connect("./data/database.db") as conn:
-        results = conn.execute(
-            """SELECT DISTINCT Lang, MiniID, Region, PublisherID
+        if results := conn.execute(
+            """SELECT DISTINCT Lang, MiniID || Region || COALESCE(PublisherID, '')
             FROM GameLocalePublisher
             WHERE MiniID = ?
             AND (Lang != 'JA' OR Region IN ('A', 'J'))
@@ -53,26 +53,23 @@ async def get_title_page(
             f" AND PublisherID IS{' NOT' if is_full_title_id else ''} NULL"
             " ORDER BY Region DESC",
             [title_mini_id]
-        ).fetchall()
-        
-        if results:
+        ).fetchall():
             markdown += "<tg-slideshow>\n"
             # Metti la copertina della lingua del gioco cercato come prima opzione, se presente
             if result_userlang := next((x for x in results if x[0] == lang), None):
                 results.insert(0, results.pop(results.index(result_userlang)))
-        for result_lang, result_miniID, \
-        result_region, result_publisherID in results:
-            result_titleID = f"{result_miniID}{result_region}{result_publisherID or ''}"
-            if not result_titleID in title_titleIDs:
-                title_titleIDs.append(result_titleID)
-            
-            # Controlla che tutte le copertine esistano, controllando l'head dell'url
-            cover_url = f"https://art.gametdb.com/wii/coverfullHQ/{result_lang}/{result_titleID}.png"
-            if not await fetch_url_head(cover_url):
-                continue
-            
-            markdown += f"![]({cover_url})"
-        else:
+
+            for result_lang, result_titleID in results:
+                if not result_titleID in title_titleIDs:
+                    title_titleIDs.append(result_titleID)
+                
+                # Controlla che tutte le copertine esistano, controllando l'head dell'url
+                cover_url = f"https://art.gametdb.com/wii/coverfullHQ/{result_lang}/{result_titleID}.png"
+                if not await fetch_url_head(cover_url):
+                    continue
+                
+                markdown += f"![]({cover_url})"
+
             markdown += "</tg-slideshow>\n\n"
         
         # Aggiungi fallback alle lingue per titolo e sinossi
@@ -89,29 +86,33 @@ async def get_title_page(
                 markdown += f"# {result_title}{'[^EN]' if test_lang == 'JA' else ''}\n"
                 markdown += f"<sup>[{', '.join(f'=={x}==' if x == title_id else x for x in title_titleIDs)}](https://wiki.dolphin-emu.org/index.php?title={title_id})</sup>\n"
                 if result_synopsis: markdown += f"<details><summary>Synopsis</summary>\n> {result_synopsis.replace('\n', '\n>')}</details>\n"
-                
+
                 # Ottieni il nome in tutte le altre lingue
-                results = conn.execute(
+                if results := conn.execute(
                     """SELECT DISTINCT Lang, Region, Title
                     FROM GameLocalePublisher
                     WHERE MiniID = ? AND NOT (Lang = ? AND Region = ?)"""
                     f" AND PublisherID IS{' NOT' if is_full_title_id else ''} NULL"
                     " ORDER BY Region DESC",
                     [title_mini_id, test_lang, test_region]
-                ).fetchall()
-
-                # Presumibilmente EN-J viene prima di JA, per questo il ORDER BY Region DESC
-                japanese_transliteration: str | None = None
-                if results: markdown += f"<details><summary>Name in other languages</summary>\n"
-                for result_lang, result_region, result_title in results:
-                    if result_lang == 'EN' and result_region == 'J':
-                        japanese_transliteration = result_title
-                    if not (result_flag := LANG_FLAGS.get((result_lang, result_region))):
-                        continue
+                ).fetchall():
+                    if japanese_transliteration := [
+                        x[2] for x in results
+                        if x[0] == 'EN' and x[1] == 'J'    
+                    ]:
+                        markdown += f"[^EN]: {japanese_transliteration[0]}\n"
                     
-                    markdown += f"{result_flag} **{result_title}**{'[^EN]' if result_lang == 'JA' else ''}\n\n"
-                else: markdown += f"{f'[^EN]: {japanese_transliteration}' if japanese_transliteration else ''}</details>\n"
-                
+                    if results := [
+                        x + (flag,) for x in results
+                        if (flag := LANG_FLAGS.get((x[0], x[1])))
+                    ]:
+                        markdown += f"<details><summary>Name in other languages</summary>\n"
+                        
+                        for result_lang, _, result_title, result_flag in results:
+                            markdown += f"{result_flag} **{result_title}**{'[^EN]' if result_lang == 'JA' else ''}\n\n"
+                        
+                        markdown += f"</details>\n\n"
+                                
                 break
     
     # TODO: continue
