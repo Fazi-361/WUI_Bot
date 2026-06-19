f0bbf613d8cbbcba74c679238eb3932dd71cd094
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-06-19 18:48:49 +0200

    Implementa una gestione migliore delle altre lingue dei giochi

--- utils/get_title_page.py
+++ utils/get_title_page.py
@@ -4,24 +4,22 @@ from aiogram.types.input_rich_message import InputRichMessage
 from async_lru import alru_cache
 from utils.fetch_url_head import fetch_cover_head
 
-LANG_FLAGS: dict[tuple[str, str], str] = {
-    ('JA', 'J'): '🇯🇵',
-    ('EN', 'P'): '🇬🇧',
-    ('US', 'E'): '🇺🇸',
-    ('DE', 'D'): '🇩🇪',
-    ('DE', 'P'): '🇩🇪',
-    ('FR', 'F'): '🇫🇷',
-    ('FR', 'P'): '🇫🇷',
-    ('IT', 'I'): '🇮🇹',
-    ('IT', 'P'): '🇮🇹',
-    ('ES', 'S'): '🇪🇸',
-    ('ES', 'P'): '🇪🇸',
-    ('KO', 'K'): '🇰🇷',
-    ('SE', 'V'): '🇸🇪',
-    ('SE', 'W'): '🇸🇪',
-    ('FI', 'V'): '🇫🇮',
-    ('ZHCN', 'W'): '🇨🇳',
-    ('ZHTW', 'W'): '🇹🇼'
+LANG_FLAGS: dict[str, str] = {
+    'JA': '🇯🇵',
+    'EN': '🇬🇧',
+    'US': '🇺🇸',
+    'DE': '🇩🇪',
+    'FR': '🇫🇷',
+    'FR': '🇫🇷',
+    'IT': '🇮🇹',
+    'ES': '🇪🇸',
+    'KO': '🇰🇷',
+    'SE': '🇸🇪',
+    'FI': '🇫🇮',
+    'NL': '🇳🇱',
+    'DK': '🇩🇰',
+    'ZHCN': '🇨🇳',
+    'ZHTW': '🇹🇼'
 }
 
 
@@ -32,26 +30,25 @@ async def get_title_page(
     morphable_lang: bool = True,
 ) -> InputRichMessage:
     # timer_start: float = timer()
-    title_id_len: int = len(title_id)
-    is_full_title_id: bool = title_id_len == 6
-    title_mini_id: str = title_id[:3]
-    title_region: str = title_id[3]
-    title_titleIDs: list[str] = [title_id]
+    is_full_title_id: bool = len(title_id) == 6
+    title_titleIDs: list[str] = []
+    title_other_names: dict[str, str] = {}
     markdown: str = ""
-    
+
     cursor = get_cursor()
     # Aggiungi fallback alle lingue per titolo e sinossi,
     # cambiando il valore del parametro lang e title_region
     for lang in (lang, 'US', 'EN', 'JA'):
         if results := cursor.execute(
-            f"""SELECT Title, Synopsis
+            f"""SELECT Title, Synopsis, MiniID, Region, MiniID || Region || COALESCE(PublisherID, '')
             FROM GameLocalePublisher
             WHERE Lang = ? AND MiniID = ? {'AND Region = ?' if morphable_lang else ''}
             AND PublisherID IS{' NOT' if is_full_title_id else ''} NULL""",
-            [lang, title_mini_id] + ([title_region] if morphable_lang else [])
+            [lang, title_id[:3]] + ([title_id[3]] if morphable_lang else [])
         ).fetchone():
-            result_title, result_synopsis = results
-            english_japanese = lang == 'EN' and title_region == 'J'
+            title_title, title_synopsis, title_mini_id, title_region, title_id = results
+            title_titleIDs.append(title_id)
+            english_japanese = morphable_lang and lang == 'EN' and title_region == 'J'
             break
     else:
         raise
@@ -73,32 +70,39 @@ async def get_title_page(
     else:
         raise
     
+    
+    japanese_transliteration: str = ""
     if results := cursor.execute(
-        """SELECT DISTINCT Lang, MiniID || Region || COALESCE(PublisherID, '')
+        """SELECT DISTINCT Lang, Region, Title, MiniID || Region || COALESCE(PublisherID, '')
         FROM GameLocalePublisher
         WHERE MiniID = ? AND Type = ?
         AND (Lang != 'JA' OR Region IN ('A', 'J'))
-        AND ((Lang != 'EN' AND Lang != 'US') OR Region IN ('A', 'P', 'E', 'N'))
-        AND (Lang != 'DE' OR Region IN ('A', 'D', 'P', 'L', 'M'))
-        AND (Lang != 'FR' OR Region IN ('A', 'F', 'P', 'L', 'M'))
-        AND (Lang != 'IT' OR Region IN ('A', 'I', 'P', 'L', 'M'))
-        AND (Lang != 'ES' OR Region IN ('A', 'S', 'P', 'L', 'M'))
+        AND (Lang != 'US' OR Region IN ('A', 'E', 'N', 'X', 'Y', 'Z'))
+        AND (Lang != 'EN' OR Region IN ('A', 'P', 'H', 'U', 'X', 'Y', 'Z', 'J'))
+        AND (Lang != 'DE' OR Region IN ('A', 'D', 'P', 'L', 'M', 'H', 'U', 'X', 'Y', 'Z'))
+        AND (Lang != 'FR' OR Region IN ('A', 'F', 'P', 'L', 'M', 'H', 'U', 'X', 'Y', 'Z'))
+        AND (Lang != 'IT' OR Region IN ('A', 'I', 'P', 'L', 'M', 'H', 'U', 'X', 'Y', 'Z'))
+        AND (Lang != 'ES' OR Region IN ('A', 'S', 'P', 'L', 'M', 'H', 'U', 'X', 'Y', 'Z'))
         AND (Lang != 'KO' OR Region IN ('A', 'K', 'Q', 'T'))
         AND ((Lang != 'SE' AND Lang != 'FI') OR Region IN ('V', 'W'))
         AND ((Lang != 'ZHCN' AND Lang != 'ZHTW') OR Region = 'W')
         ORDER BY Region DESC""",
         [title_mini_id, title_type]
     ).fetchall():
-        markdown += "<tg-slideshow>\n"
-
         # Metti la copertina della lingua del gioco cercato come prima opzione, se presente
         cover_lang: str = "JA" if english_japanese else lang
         if result_userlang := next((x for x in results if x[0] == cover_lang), None):
             results.insert(0, results.pop(results.index(result_userlang)))
 
-        for result_lang, result_titleID in results:
+        markdown += "<tg-slideshow>\n"
+        for result_lang, result_region, result_title, result_titleID in results:
             if not result_titleID in title_titleIDs:
                 title_titleIDs.append(result_titleID)
+
+            if result_lang == 'EN' and result_region == 'J':
+                japanese_transliteration = result_title
+            elif result_lang != lang:
+                title_other_names[result_lang] = result_title
             
             # Controlla che tutte le copertine esistano, controllando l'head dell'url
             if not await fetch_cover_head(resource := f"{result_lang}/{result_titleID}"):
@@ -109,7 +113,7 @@ async def get_title_page(
         markdown += "</tg-slideshow>\n"
     
     markdown += (
-        f"# {result_title}{'[^EN]' if lang == 'JA' else ''}\n"
+        f"# {title_title}{'[^EN]' if lang == 'JA' else ''}\n"
         f"<sup>[{', '.join(f'=={x}==' if x == title_id else x for x in title_titleIDs)}](https://wiki.dolphin-emu.org/index.php?title={title_id})</sup>\n"
         "\n"
         f"**Developer**: {title_developer}  \n"
@@ -117,8 +121,11 @@ async def get_title_page(
         f"**Released**: ![{title_release_date}](tg://time?unix={title_release_unix}&format=D)\n\n"
     )
     
-    if result_synopsis:
-        markdown += f"<details><summary>Synopsis</summary>\n> {result_synopsis.replace('\n', '\n> ')}</details>\n"
+    if japanese_transliteration:
+        markdown += f"[^EN]: {japanese_transliteration}\n"
+    
+    if title_synopsis:
+        markdown += f"<details><summary>Synopsis</summary>\n> {title_synopsis.replace('\n', '\n> ')}</details>\n"
 
     if results := cursor.execute(
         """SELECT Version, CRC, MD5, SHA1
@@ -138,30 +145,13 @@ async def get_title_page(
         markdown += "</details>\n"
 
     # Ottieni il nome in tutte le altre lingue
-    if results := cursor.execute(
-        """SELECT DISTINCT Lang, Region, Title
-        FROM GameLocale
-        WHERE MiniID = ? AND NOT (Lang = ? AND Region = ?)
-        AND Type = ?
-        ORDER BY Region DESC""",
-        [title_mini_id, lang, title_region, title_type]
-    ).fetchall():
-        if japanese_transliteration := [
-            x[2] for x in results
-            if x[0] == 'EN' and x[1] == 'J'    
-        ]:
-            markdown += f"[^EN]: {japanese_transliteration[0]}\n"
+    if title_other_names:
+        markdown += "<details><summary>Name in other languages</summary>\n"
         
-        if results := [
-            x + (flag,) for x in results
-            if (flag := LANG_FLAGS.get((x[0], x[1])))
-        ]:
-            markdown += "<details><summary>Name in other languages</summary>\n"
-            
-            for result_lang, _, result_title, result_flag in results:
-                markdown += f"{result_flag} **{result_title}**{'[^EN]' if result_lang == 'JA' and not english_japanese else ''}\n\n"
-            
-            markdown += "</details>\n"
+        for result_lang, result_title in title_other_names.items():
+            markdown += f"{LANG_FLAGS[result_lang]} **{result_title}**{'[^EN]' if result_lang == 'JA' and not english_japanese else ''}\n\n"
+        
+        markdown += "</details>\n"
     
     # markdown += f"<sub>*generato in {timer() - timer_start:.3f} secondi*</sub>"
     
