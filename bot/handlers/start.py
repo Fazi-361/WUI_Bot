661917f61197585edd4f1c4baa5ff9a6464da9bf
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-08-18 00:04:13 +0200

    whoopsie daisy

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -5,10 +5,9 @@ from aiogram.utils.i18n import I18n
 from bot.filters.command import CustomCommand
 
 start_router: Router = Router()
-start_router.message.filter(CustomCommand("info"))
 
 
-@start_router.message(CustomCommand('start'))
+@start_router.message(CustomCommand("start"))
 async def start(message: Message, i18n: I18n) -> None:
     await message.reply(f"{i18n.gettext("start.hello")}\n\n🇺🇸 To change language, use /settings")
     
