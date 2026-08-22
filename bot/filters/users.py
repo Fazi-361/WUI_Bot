88e92124940d550871b88d0a11d0084a70053286
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-08-22 18:07:28 +0200

    [TEMP] Gestisci admin anonimi

--- bot/filters/users.py
+++ bot/filters/users.py
@@ -3,6 +3,7 @@ from aiogram.filters import BaseFilter
 from aiogram.types import (
     CallbackQuery,
     ChatMemberAdministrator,
+    ChatMemberLeft,
     ChatMemberOwner,
     Message,
 )
@@ -11,18 +12,20 @@ from aiogram.types import (
 class Administrator(BaseFilter):
     async def __call__(self, message: Message | CallbackQuery) -> bool:
         try:
-            chat = message.chat if isinstance(message, Message) else message.message.chat # type: ignore
+            chat = message.chat if isinstance(message, Message) else message.message.chat  # type: ignore
 
             if chat.type == ChatType.PRIVATE:
                 return True
-            
-            member = await chat.get_member(message.from_user.id) # type: ignore
+
+            member = await chat.get_member(message.from_user.id)  # type: ignore
             match member:
                 case ChatMemberOwner():
                     return True
                 case ChatMemberAdministrator():
                     return member.can_manage_chat
+                case ChatMemberLeft():
+                    return member.user.id == 1087968824  # GroupAnonymousBot
         except:
             pass
-            
+
         return False
