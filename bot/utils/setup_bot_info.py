79914029d645a7e6e96569b8b6d2671eed3e8263
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-08-28 17:57:29 +0200

    Usa il context manager

--- bot/utils/setup_bot_info.py
+++ bot/utils/setup_bot_info.py
@@ -21,71 +21,70 @@ async def setup_bot_info(bot: Bot, i18n: I18n) -> None:
             continue
 
         print("Setting bot info for", locale)
+        with i18n.use_locale(locale):
+            language_code: str | None = locale.lower() if locale != default_locale else None
 
-        __ = lambda singular: _(singular, locale=locale)
-        language_code: str | None = locale.lower() if locale != default_locale else None
+            # * Info bot
 
-        # * Info bot
+            try:
+                if bot_name_def != (new_name := _("bot.name")):
+                    await bot.set_my_name(new_name, language_code)
+            except:
+                pass
 
-        try:
-            if bot_name_def != (new_name := __("bot.name")):
-                await bot.set_my_name(new_name, language_code)
-        except:
-            pass
+            await bot.set_my_description(_("bot.description"), language_code)
+            await bot.set_my_short_description(_("bot.short_description"), language_code)
 
-        await bot.set_my_description(__("bot.description"), language_code)
-        await bot.set_my_short_description(__("bot.short_description"), language_code)
+            # * Comandi
 
-        # * Comandi
+            for command in BOTCOMMANDS:
+                command.description = _(f"command.{command.command}.description")
 
-        for command in BOTCOMMANDS:
-            command.description = __(f"command.{command.command}.description")
+            # Gruppi
 
-        # Gruppi
+            help_botcommand.is_ephemeral = True
+            settings_botcommand.is_ephemeral = True
 
-        help_botcommand.is_ephemeral = True
-        settings_botcommand.is_ephemeral = True
+            # Gruppi: non admin
+            await bot.set_my_commands(
+                commands=[
+                    help_botcommand,
+                    info_botcommand,
+                    deid_botcommand,
+                    id_botcommand
+                ],
+                language_code=language_code,
+            )
 
-        # Gruppi: non admin
-        await bot.set_my_commands(
-            commands=[
-                help_botcommand,
-                info_botcommand,
-                deid_botcommand,
-                id_botcommand
-            ],
-            language_code=language_code,
-        )
+            # Gruppi: admin
+            await bot.set_my_commands(
+                commands=[
+                    help_botcommand,
+                    settings_botcommand,
+                    info_botcommand,
+                    deid_botcommand,
+                    id_botcommand
+                ],
+                scope=BotCommandScopeAllChatAdministrators(),
+                language_code=language_code,
+            )
 
-        # Gruppi: admin
-        await bot.set_my_commands(
-            commands=[
-                help_botcommand,
-                settings_botcommand,
-                info_botcommand,
-                deid_botcommand,
-                id_botcommand
-            ],
-            scope=BotCommandScopeAllChatAdministrators(),
-            language_code=language_code,
-        )
+            # Chat private
 
-        # Chat private
+            help_botcommand.is_ephemeral = False
+            settings_botcommand.is_ephemeral = False
 
-        help_botcommand.is_ephemeral = False
-        settings_botcommand.is_ephemeral = False
-
-        await bot.set_my_commands(
-            commands=[
-                help_botcommand,
-                settings_botcommand,
-                info_botcommand,
-                deid_botcommand,
-                id_botcommand
-            ],
-            scope=BotCommandScopeAllPrivateChats(),
-            language_code=language_code,
-        )
+            await bot.set_my_commands(
+                commands=[
+                    help_botcommand,
+                    settings_botcommand,
+                    info_botcommand,
+                    deid_botcommand,
+                    id_botcommand
+                ],
+                scope=BotCommandScopeAllPrivateChats(),
+                language_code=language_code,
+            )
 
         # ? Rate limit ≈ 30r/s
         await sleep(2)
