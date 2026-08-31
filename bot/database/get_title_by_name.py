589f71a574d27e16330d15a9c9c99f7ecb812b60
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-08-31 21:41:11 +0200

    CESTINA jaro_winkler (grazie) ma solo dalla prima parte dell'algoritmo

--- bot/database/get_title_by_name.py
+++ bot/database/get_title_by_name.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-from jellyfish import jaro_winkler_similarity as jws
+from jellyfish import jaro_winkler_similarity as jw, jaccard_similarity as ja
 from unidecode import unidecode_expect_nonascii as unidecode
 
 from . import CONNECTION, use_cursor
@@ -11,21 +11,17 @@ from ..utils.text import strim
 def get_title_by_name(input: str, region: str = "P") -> tuple[str, str, str] | None:
     input = unidecode(strim(input.upper()), errors="preserve")
     input_split: list[str] = input.split()
-    treshold: float = 0.82
 
     def similarity(str1: str) -> float:
         if input == (str1 := unidecode(str1, errors="preserve")):
             return 100
 
-        similarity: float = jws(input, str1, True)
-        if similarity < treshold:
-            return 0
-
+        similarity: float = ja(str1, input)
         str1_split: list[str] = str1.split()
         highest_index: int = 0
         for word2 in input_split:
             for i, word1 in enumerate(str1_split[highest_index:], 1):
-                if (word_similarity := jws(word2, word1)) >= treshold:
+                if (word_similarity := jw(word2, word1)) >= .8:
                     highest_index += i
                     similarity += word_similarity
                     break
@@ -59,5 +55,5 @@ def get_title_by_name(input: str, region: str = "P") -> tuple[str, str, str] | N
             OR NOT 'P' IN Regions AND Region = 'J'
             OR NOT 'J' IN Regions
             LIMIT 1""",
-            [(1 - treshold) + len(input_split) * 0.9, region, region],
+            [len(input_split) * .9 + .2, region, region],
         ).fetchone()
