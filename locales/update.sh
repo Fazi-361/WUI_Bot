37fcb9a2e0c630c828202e1525b6749c36f589b5
Laxan3000 <58862419+Laxan3000@users.noreply.github.com>
2026-09-13 15:43:36 +0200

    Riprova aggiornando i file di lingua

--- locales/update.sh
+++ locales/update.sh
@@ -1,3 +1,4 @@
 pybabel extract -k _:1,1t -k _:1,2 -k __ --input-dirs=. -o locales/messages.pot --no-location --project=WiiWiiUBot
-pybabel update -d locales -D messages -i locales/messages.pot -N --ignore-obsolete --omit-header
+pybabel update -l US -d locales -D messages -i locales/messages.pot -N --ignore-obsolete --ignore-pot-creation-date
+pybabel update -l IT -d locales -D messages -i locales/messages.pot -N --ignore-obsolete --ignore-pot-creation-date
 echo "Now update the language files!"
